7befe5363a80a394795cc164afd4ecd3dc95f6d1
Ace Huang <Ace@Aces-mac.local>
2021-01-30 01:33:27 -0500

    create bottom nav

--- DolphinDive/app/src/main/java/com/capstone/dolphindive/DiveShop.java
+++ DolphinDive/app/src/main/java/com/capstone/dolphindive/DiveShop.java
@@ -1,14 +1,19 @@
 package com.capstone.dolphindive;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class DiveShop extends AppCompatActivity {
+public class DiveShop extends Fragment {
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dive_shop);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_dive_shop, container, false);
     }
 }
