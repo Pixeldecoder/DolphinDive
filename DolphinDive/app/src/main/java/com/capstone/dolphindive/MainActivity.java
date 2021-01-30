cfc4e93f4fffaa7feed4decb66652d73255bb60b
Ace Huang <Ace@Aces-mac.local>
2021-01-30 02:44:52 -0500

    fixed bugs, updated nav bar, starting page

--- DolphinDive/app/src/main/java/com/capstone/dolphindive/MainActivity.java
+++ DolphinDive/app/src/main/java/com/capstone/dolphindive/MainActivity.java
@@ -14,20 +14,12 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, PreStart.class);
-                startActivity(intent);
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+
        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
         if (savedInstanceState == null){
