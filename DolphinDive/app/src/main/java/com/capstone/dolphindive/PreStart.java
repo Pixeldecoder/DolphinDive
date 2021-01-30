e8c4ec5427053aa96a4e8ba0eecc111cfc5aebf4
Ace Huang <Ace@Aces-mac.local>
2021-01-30 03:02:50 -0500

    fixed bugs, order starting page add styles and layouts

--- DolphinDive/app/src/main/java/com/capstone/dolphindive/PreStart.java
+++ DolphinDive/app/src/main/java/com/capstone/dolphindive/PreStart.java
@@ -2,13 +2,23 @@ package com.capstone.dolphindive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class PreStart extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_start);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(PreStart.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
