a3c5ccbe9b74840e1bdcec242c3fd56b9b49d80d
Susan Chen <chenshuying1998@hotmail.com>
2021-01-31 15:39:07 -0500

    Update dive log layout

--- DolphinDive/app/src/main/java/com/capstone/dolphindive/DiveLog_Free.java
+++ DolphinDive/app/src/main/java/com/capstone/dolphindive/DiveLog_Free.java
@@ -4,14 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class DiveLog_Free extends AppCompatActivity {
 
