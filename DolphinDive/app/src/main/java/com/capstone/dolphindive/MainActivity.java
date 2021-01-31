781ccddfa0249249d2d299e733d3355afa893b6f
Susan Chen <chenshuying1998@hotmail.com>
2021-01-30 23:50:24 -0500

    Updated the navigation between two logs

--- DolphinDive/app/src/main/java/com/capstone/dolphindive/MainActivity.java
+++ DolphinDive/app/src/main/java/com/capstone/dolphindive/MainActivity.java
@@ -16,14 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, PreStart.class);
-                startActivity(intent);
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent = new Intent(MainActivity.this, PreStart.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        }, SPLASH_TIME_OUT);
     }
 
     public void logout(View view) {
