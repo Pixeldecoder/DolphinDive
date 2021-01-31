69683516746a91f6f644865b86873c9498c1cf4b
Freddie <yanz20@mcmaster.ca>
2021-01-30 19:59:25 -0500

    few changes on social platform

--- DolphinDive/app/src/main/java/com/capstone/dolphindive/SocialPlatform.java
+++ DolphinDive/app/src/main/java/com/capstone/dolphindive/SocialPlatform.java
@@ -13,6 +13,8 @@ public class SocialPlatform extends Fragment {
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
             return inflater.inflate(R.layout.activity_social_platform, container, false);
         }
     }
