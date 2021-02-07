0c7c0334f77c52a36c23a0ebab3ed41569c4e166
Freddie <freddieyan555@gmail.com>
2021-02-07 13:03:52 -0500

    test

--- DolphinDive/app/src/main/java/com/capstone/dolphindive/MainActivity.java
+++ DolphinDive/app/src/main/java/com/capstone/dolphindive/MainActivity.java
@@ -14,6 +14,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+ 
 
     Button chatBtn;
 
