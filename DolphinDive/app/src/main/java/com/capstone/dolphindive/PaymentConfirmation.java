85d277347da6236304ba491cd828dbb480c25274
Ace Huang <Ace@Aces-mac.local>
2021-03-08 22:50:14 -0500

    update diveshop and payment flow

--- DolphinDive/app/src/main/java/com/capstone/dolphindive/PaymentConfirmation.java
+++ DolphinDive/app/src/main/java/com/capstone/dolphindive/PaymentConfirmation.java
@@ -7,17 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class PaymentMethod extends AppCompatActivity {
+public class PaymentConfirmation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payment_method);
-        Button confirmation = findViewById(R.id.confirm_booking);
-        confirmation.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_payment_confirmation);
+        Button paymentconfirm = findViewById(R.id.Pay_confirm);
+        paymentconfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent confirmmessage = new Intent(PaymentMethod.this, PaymentConfirmation.class);
+                Intent confirmmessage = new Intent(PaymentConfirmation.this, MainActivity.class);
                 startActivity(confirmmessage);
             }
         });
